d2fe3e0ed5c76c7d7c94780ade8e8ebdb85cb6b8
davidwp009 <davydperepichka2009@gmail.com>
2026-02-09 13:22:29 +0200

    www

--- script.js
+++ script.js
@@ -13,3 +13,17 @@
       document.body.classList.toggle("no-scroll");
     }
   })();
+
+
+
+  const lenis = new Lenis({
+  duration: 1.2,
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
