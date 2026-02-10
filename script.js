47da65b38e063ec7749f743c3b52e1debec1a2ba
davidwp009 <davydperepichka2009@gmail.com>
2026-02-10 02:30:44 +0200

    animation

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
-  })();
+  const openModalBtns = document.querySelectorAll("[data-modal-open]");
+  const closeModalBtn = document.querySelector("[data-modal-close]");
+  const modal = document.querySelector("[data-modal]");
+
+  openModalBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      toggleModal();
+    });
+  });
+
+  closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
 
 
 
